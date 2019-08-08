70a83a086645b82da4e1e8a44a6cdf395cf5094f
WSINTRA <will.simpson85@gmail.com>
2019-08-08 17:32:38 -0400

    provider added to index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import './sass/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
-import Reducer from './redux/reducer'
-
-console.log(createStore)
+import reducer from './redux/reducer'
+import { Provider } from 'react-redux'
 
 //create a function called reducer and pass that to the store
 //whatever the reducer returns becomes state.
 
-let store = createStore(Reducer)
+let store = createStore(reducer)
+// console.log("Getting state", store.getState())
+
+store.dispatch({type:"ADD_GOLDBARS", payload: "something"}) //How we want to change state
+//Dispatch only takes actions
 console.log("Getting state", store.getState())
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,21 @@
 //reducer
-function reducer( state, action){
-	console.log(state, action)
-	return {"Hello": "name"}
+
+const initialState = {
+	goldbars: [],
+	filter: ""
+}
+
+function reducer( state = initialState , action){
+
+	switch(action.type){
+		case "ADD_GOLDBARS":
+		return {...state, goldbars: [...state.goldbars, {bar: action.payload} ]}
+		break;
+		default:
+		return state
+	}	
 }
 
 export default reducer;
+
+
